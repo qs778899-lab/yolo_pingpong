c6bb075b2c1ad1d64b630bb6389af60cf8841282
qs778899-lab <xinyazhong688@gmail.com>
2026-02-13 11:18:20 +0800

    add pipline pdf

--- train.py
+++ train.py
@@ -1,4 +1,6 @@
 from ultralytics import YOLO
+import torch 
+import os
 # 限制 PyTorch 内部计算使用的 CPU 线程数 (建议设为 2 或 4)
 torch.set_num_threads(6)
 # 限制底层库 (如 OpenBLAS, MKL) 使用的线程数
@@ -7,12 +9,9 @@ os.environ["MKL_NUM_THREADS"] = "6"
 
 # conda activate yolov11 && python train.py
 
-model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
-
+model = YOLO("yolo11n.pt")  # load a pretrained model
 # 加载你之前的阶段性成果
 # model = YOLO("runs/detect/train7/weights/epoch50.pt")
-
 # Train the model
 results = model.train(data="/home/s114/yolo/dataset/pingpong.yaml", epochs=500, imgsz=960, device=0, cache=False, plots=True, save_period=20, workers=4, batch=8, name="pingpong")
-
 # results = model.train(data="/home/s114/yolo/dataset/pingpong.yaml", epochs=30, imgsz=640, device='cpu')
